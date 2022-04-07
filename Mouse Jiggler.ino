6bb41d40d3b093e9381af25249bed30c8b6a0e38
Eduardo Arballo <51279560+EduardoArballo27@users.noreply.github.com>
2022-04-07 09:06:57 -0700

    Update Mouse Jiggler.ino

--- Mouse Jiggler.ino
+++ Mouse Jiggler.ino
@@ -10,13 +10,6 @@ void loop() {
   DigiMouse.delay(50);
   DigiMouse.moveY(-1);
   
-  //Blinks LED light for half a second (delete this if too annoying)
-  digitalWrite(1,LOW);
-  DigiKeyboard.delay(50);
-  digitalWrite(1,HIGH);
-  DigiKeyboard.delay(50);
-  digitalWrite(1,LOW);
-  
   //Waits one minute before repeating
   DigiMouse.delay(60000);
 }
